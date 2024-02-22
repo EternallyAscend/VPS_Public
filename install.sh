bb7d607610481aad4ebafd9e442338da49433f2c
hlyu <hlyu@MacBook14P.local>
2024-02-22 23:18:49 +0800

    Update xui configs.

---
+++ install.sh
@@ -0,0 +1,14 @@
+apt update -y
+apt upgrade -y
+
+apt install docker docker-compose nginx git -y
+
+# docker pull zhayujie/chatgpt-on-wechat
+# docker pull lobehub/lobe-chat
+# docker pull justsong/one-api
+docker pull yidadaa/chatgpt-next-web
+docker pull mysql
+docker pull enwaiax/x-ui
+docker pull nginx
+
+
